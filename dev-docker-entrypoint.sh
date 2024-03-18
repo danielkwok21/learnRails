3061472a98917065bd323240d7193d80a5d9f319
daniel <danielzhengxian21@gmail.com>
2024-03-18 18:49:44 +0800

    implemnting docker + postgres

---
+++ dev-docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+echo "ENVIRONMENT: $RAILS_ENV"
+
+# check bundle
+bundle check || bundle install
+
+# remove pid from prev session (puma server)
+rm -rf $APP_PATH/tmp/pids/server.pid
+
+# run anything by prepending bundle exec to the passed command
+bundle exec ${@}
