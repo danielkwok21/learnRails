2a3e08d96689f17af1aa8a23abb3f07cd63aa7d4
daniel <danielzhengxian21@gmail.com>
2024-03-19 10:52:42 +0800

    mvp postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_17_034137) do
+ActiveRecord::Schema.define(version: 2024_03_17_055300) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
