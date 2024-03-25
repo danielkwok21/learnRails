04ff242ea6410c367b5311494b01f2f4411b37cd
daniel <danielzhengxian21@gmail.com>
2024-03-25 22:54:42 +0800

    clean up

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,4 @@
 class MainController < ApplicationController 
     def index
-        @some_secret_key=ENV['SOME_SECRET_KEY']
-        
-        ipAPI =  IpApi.new()
-        @response = ipAPI.get_details_by_ip(request.remote_ip)
     end
 end
